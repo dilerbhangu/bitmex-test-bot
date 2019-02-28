32c2b6baaa515eb1d29077d5441af0c912fa6f99
diler <dilersingh96@yahoo.com>
2019-02-28 21:21:22 +0530

    commit

--- trader.py
+++ trader.py
@@ -1,6 +1,6 @@
 
 class Trader():
-    def __init__(self, client, strategy,ohlcv_candles, money_to_trade=100, leverage=5):
+    def __init__(self, client, strategy, ohlcv_candles, money_to_trade=100, leverage=5):
         self.client = client
         self.strategy = strategy
         self.money_to_trade = money_to_trade
@@ -21,31 +21,30 @@ class Trader():
                     symbol="XBTUSD", side="Buy", orderQty=self.money_to_trade * self.leverage).result()
                 return response
             else:
-                response=None
+                response = None
                 return response
 
         except Exception as e:
             print('something goes wrong')
 
-        return 
+        return
 
-    def set_stop_limit(self):
-            if response[0]['side']=='Buy':
-                 stop_order_response=self.client.Order.Order_new(
-                    symbol="XBTUSD", side="Sell", orderQty=self.money_to_trade * self.leverage,stopPx=exec_price-7,price=exec_price-10).result()
-            elif response[0]['side']=='Sell':
-                 stop_order_response=self.client.Order.Order_new(
-                    symbol="XBTUSD", side="Buy", orderQty=self.money_to_trade * self.leverage,stopPx=exec_price+7,price=exec_price+10).result()
+    def set_stop_limit(self, exec_price):
+        if response[0]['side'] == 'Buy':
+            stop_order_response = self.client.Order.Order_new(
+                symbol="XBTUSD", side="Sell", orderQty=self.money_to_trade * self.leverage, stopPx=exec_price-7, price=exec_price-10).result()
+        elif response[0]['side'] == 'Sell':
+            stop_order_response = self.client.Order.Order_new(
+                symbol="XBTUSD", side="Buy", orderQty=self.money_to_trade * self.leverage, stopPx=exec_price+7, price=exec_price+10).result()
 
-            return stop_order_response
+        return stop_order_response
 
+    def set_take_profit(self, exec_price):
+        if response[0]['side'] == 'Buy':
+            take_profit_order_response = self.client.Order.Order_new(
+                symbol="XBTUSD", side="Sell", orderQty=self.money_to_trade * self.leverage, stopPx=exec_price+15, price=exec_price+20, ordType='LimitIfTouched').result()
+        elif response[0]['side'] == 'Sell':
+            take_profit_order_response = self.client.Order.Order_new(
+                symbol="XBTUSD", side="Buy", orderQty=self.money_to_trade * self.leverage, stopPx=exec_price-15, price=exec_price-20, ordType='LimitIfTouched').result()
 
-    def set_take_profit(self):
-            if response[0]['side']=='Buy':
-                 take_profit_order_response=self.client.Order.Order_new(
-                    symbol="XBTUSD", side="Sell", orderQty=self.money_to_trade * self.leverage,stopPx=exec_price+15,price=exec_price+20,ordType='LimitIfTouched').result()
-            elif response[0]['side']=='Sell':
-                 take_profit_order_response=self.client.Order.Order_new(
-                    symbol="XBTUSD", side="Buy", orderQty=self.money_to_trade * self.leverage,stopPx=exec_price-15,price=exec_price-20,ordType='LimitIfTouched').result()
-
-            return take_profit_order_response
+        return take_profit_order_response
